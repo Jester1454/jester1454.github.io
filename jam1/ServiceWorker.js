0960e39230a2afc0aca8237fb94e425980b9e4da
Khokhlov Mikhail <jester1454@gmail.com>
2023-07-09 20:06:35 +0300

    asdljiaskldj

---
+++ jam1/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Ded Reflection-Bullet Catcher-0.1";
+const contentToCache = [
+    "Build/333.loader.js",
+    "Build/333.framework.js",
+    "Build/333.data",
+    "Build/333.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
